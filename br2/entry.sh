cf583b5fd9e4b129a278a9f83664d62225c2d206
Yoshimasa Obayashi <obayashi.yoshimasa@seek-the-truth.org>
2020-08-24 07:58:07 +0900

    disable bash login for debug

--- br2/entry.sh
+++ br2/entry.sh
@@ -9,5 +9,5 @@ ${WORKING_DIR_BR2}/setup.sh 2>&1 | tee ${LOG_DIR_BR2}/log-setup.txt
 # build
 ${WORKING_DIR_BR2}/build.sh 2>&1 | tee ${LOG_DIR_BR2}/log-build.txt
 
-# exec bash
-bash
+# exec bash for debug
+#bash
